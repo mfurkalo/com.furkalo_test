cfaa8b59e52a5dfe200d94f2c4734f4e04c96634
Maksym <maximkack@gmail.com>
2021-02-17 16:15:20 +0200

    [=] The url was corrected

--- src/main/java/data/BrainConstants.java
+++ src/main/java/data/BrainConstants.java
@@ -1,7 +1,7 @@
 package data;
 
 public class BrainConstants {
-    public  static final String BASE_URL ="https://brain.com.ua/ukr/";
+    public  static final String BASE_URL ="https://brain.com.ua/";
     public static final String LOGIN = "+38 (066) 305-12-42";
     public static final String WRONG_LOGIN = "+38 (066) 305-12-41";
     public static final String PASSWORD = "07113727";
